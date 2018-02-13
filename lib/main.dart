843b95ecb446bc69485b34e420eb5287bdca99b7
katelynwang <wang.ke2@husky.neu.edu>
2018-02-13 15:08:17 -0500

    add right side drawer for every page

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'RiskMonitor.dart';
 import 'ActivityMonitor.dart';
 import 'RiskHistory.dart';
-//import 'Material/Menu.dart';
+import 'RiskHistory_All.dart';
+import 'RiskHistory_Hypoglycemia.dart';
+import 'RiskHistory_Pneumothorax.dart';
+import 'RiskHistory_Hypothermia.dart';
 
 void main() {
   runApp(
@@ -13,12 +16,16 @@ void main() {
           routes: <String, WidgetBuilder>{
             //home page is automatically defiend as:
             //"/": (BuildContext context) => new MenuPage(),
-            "/CurrentStatePage": (BuildContext context) => new CurrentStatePage(),
+            "/CurrentState": (BuildContext context) => new CurrentStatePage(),
             "/RiskMonitor": (BuildContext context) => new RiskMonitor(),
             "/ActivityMonitor": (BuildContext context) => new ActivityMonitor(),
             "/RiskHistory": (BuildContext context) => new RiskHistory(),
             "/LabWork": (BuildContext context) => new LabWork(),
             "/PSSATForm": (BuildContext context) => new PSSATForm(),
+            "/RiskHistory_All": (BuildContext context) => new RiskHistory_All(),
+            "/RiskHistory_Hypoglycemia": (BuildContext context) => new RiskHistory_Hypoglycemia(),
+            "/RiskHistory_Pneumothorax": (BuildContext context) => new RiskHistory_Pneumothorax(),
+            "/RiskHistory_Hypothermia": (BuildContext context) => new RiskHistory_Hypothermia(),
           }
       )
   );
@@ -39,31 +46,58 @@ class MenuPage extends StatelessWidget {
               ),
 
               //current state
-              const ListTile(title: const Text('Current State')),
+              const ListTile(
+                title: const Text('Current State'),
+//                onTap: () => Navigator.pushNamed(context, "/CurrentState"),
+              ),
 
               //risk monitor
               new ExpansionTile(
                   title: const Text('Risk Monitor'),
                   backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
                   children: const <Widget>[
-                    const ListTile(title: const Text('Hypotension')),
-                    const ListTile(title: const Text('Hypothermia')),
-                    const ListTile(title: const Text('Pneumothorax'))
+                    const ListTile(
+                      title: const Text('Hypotension'),
+//                      onTap: ,
+                    ),
+                    const ListTile(
+                      title: const Text('Hypothermia'),
+//                      onTap: ,
+                    ),
+                    const ListTile(
+                      title: const Text('Pneumothorax'),
+//                      onTap: ,
+                    )
                   ]
               ),
 
               //activity monitor
-              const ListTile(title: const Text('Activity Monitor')),
+              const ListTile(
+                title: const Text('Activity Monitor'),
+//                onTap: ,
+              ),
 
               //risk history
               new ExpansionTile(
                 title: const Text('Risk Hisotry'),
                 backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
                 children: <Widget>[
-                  const ListTile(title: const Text('All')),
-                  const ListTile(title: const Text('Hypotension')),
-                  const ListTile(title: const Text('Hypothermia')),
-                  const ListTile(title: const Text('Pneumothorax'))
+                  const ListTile(
+                    title: const Text('All'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Hypoglycemia'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Pneumothorax'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Hypothermia'),
+//                      onTap: ,
+                  )
                 ],
               ),
 
@@ -72,11 +106,26 @@ class MenuPage extends StatelessWidget {
                 title: new Text('Lab Work'),
                 backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
                 children: <Widget>[
-                  const ListTile(title: const Text('View Recommended Test')),
-                  const ListTile(title: const Text('Order Additional Test')),
-                  const ListTile(title: const Text('Enter Patient Report')),
-                  const ListTile(title: const Text('View Patient Report')),
-                  const ListTile(title: const Text('Enter Recommended Medication')),
+                  const ListTile(
+                    title: const Text('View Recommended Test'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Order Additional Test'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Enter Patient Report'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('View Patient Report'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Enter Recommended Medication'),
+//                      onTap: ,
+                  )
                 ],
               ),
 
@@ -88,12 +137,30 @@ class MenuPage extends StatelessWidget {
                 title: new Text('S.T.A.B.L.E'),
                 backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
                 children: <Widget>[
-                  const ListTile(title: const Text('Sugar')),
-                  const ListTile(title: const Text('Temperature')),
-                  const ListTile(title: const Text('Airway')),
-                  const ListTile(title: const Text('Blood Pressure')),
-                  const ListTile(title: const Text('Lab Work')),
-                  const ListTile(title: const Text('Emotional Support')),
+                  const ListTile(
+                    title: const Text('Sugar'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Temperature'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Airway'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Blood Pressure'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Lab Work'),
+//                      onTap: ,
+                  ),
+                  const ListTile(
+                    title: const Text('Emotional Support'),
+//                      onTap: ,
+                  )
                 ],
               )
             ],
@@ -178,71 +245,10 @@ class CurrentStatePage extends StatelessWidget{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Current Status', textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
       ),
     );
   }
 }
-
-//class RiskMonitor extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context){
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text("Risk Monitor", textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-//        actions: <Widget>[
-//          new IconButton(
-//            icon: new Icon(Icons.search),
-//            tooltip: 'Search',
-//            onPressed: null,
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//}
-
-//class ActivityMonitor extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context){
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text("Activity Monitor", textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-//        actions: <Widget>[
-//          new IconButton(
-//            icon: new Icon(Icons.search),
-//            tooltip: 'Search',
-//            onPressed: null,
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//}
-
-//class RiskHistory extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context){
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text("Risk History", textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-//        actions: <Widget>[
-//          new IconButton(
-//            icon: new Icon(Icons.search),
-//            tooltip: 'Search',
-//            onPressed: null,
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//}
 
 class LabWork extends StatelessWidget{
   @override
@@ -250,13 +256,6 @@ class LabWork extends StatelessWidget{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Lab Work", textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
       ),
     );
   }
@@ -268,13 +267,6 @@ class PSSATForm extends StatelessWidget{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("PSSAT Form", textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
       ),
     );
   }
