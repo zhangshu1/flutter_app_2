86f6b38f166b4da7959074fa7555b6dbd903916e
katelynwang <wang.ke2@husky.neu.edu>
2018-02-13 13:34:12 -0500

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,24 @@ import 'package:flutter/material.dart';
 import 'RiskMonitor.dart';
 import 'ActivityMonitor.dart';
 import 'RiskHistory.dart';
+//import 'Material/Menu.dart';
 
 void main() {
   runApp(
-    new MaterialApp(
-      title: 'Menu Page',
-      home: new MenuPage(),
-      routes: <String, WidgetBuilder>{
-        //home page is automatically defiend as:
-        //"/": (BuildContext context) => new MenuPage(),
-        "/CurrentStatePage": (BuildContext context) => new CurrentStatePage(),
-        "/RiskMonitor": (BuildContext context) => new RiskMonitor(),
-        "/ActivityMonitor": (BuildContext context) => new ActivityMonitor(),
-        "/RiskHistory": (BuildContext context) => new RiskHistory(),
-        "/LabWork": (BuildContext context) => new LabWork(),
-        "/PSSATForm": (BuildContext context) => new PSSATForm()
-      }
-    )
+      new MaterialApp(
+          title: 'Menu Page',
+          home: new MenuPage(),
+          routes: <String, WidgetBuilder>{
+            //home page is automatically defiend as:
+            //"/": (BuildContext context) => new MenuPage(),
+            "/CurrentStatePage": (BuildContext context) => new CurrentStatePage(),
+            "/RiskMonitor": (BuildContext context) => new RiskMonitor(),
+            "/ActivityMonitor": (BuildContext context) => new ActivityMonitor(),
+            "/RiskHistory": (BuildContext context) => new RiskHistory(),
+            "/LabWork": (BuildContext context) => new LabWork(),
+            "/PSSATForm": (BuildContext context) => new PSSATForm(),
+          }
+      )
   );
 }
 
@@ -28,82 +29,145 @@ class MenuPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Scaffold is a layout for the major Material Design widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: new Text('Options', textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-
-      // body is the majority of the screen.
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+        drawer: new Drawer(
+          child: new ListView(
             children: <Widget>[
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-//                  child: new Icon(Icons.note),
-                  child: new Text("Current State", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/CurrentStatePage");}
-                )
+              //DrawerHeader for future use (dashboard, login page etc.)
+              new DrawerHeader(
+                child: new Text('Menu Options'),
+                padding: new EdgeInsets.only(left: 10.0, right: 50.0, top: 3.0, bottom: 0.0),
               ),
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-                  child: new Text("Risk Monitor", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/RiskMonitor");},
-                ),
+
+              //current state
+              const ListTile(title: const Text('Current State')),
+
+              //risk monitor
+              new ExpansionTile(
+                  title: const Text('Risk Monitor'),
+                  backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+                  children: const <Widget>[
+                    const ListTile(title: const Text('Hypotension')),
+                    const ListTile(title: const Text('Hypothermia')),
+                    const ListTile(title: const Text('Pneumothorax'))
+                  ]
               ),
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-                  child: new Text("Activity Monitor", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/ActivityMonitor");}
-                ),
+
+              //activity monitor
+              const ListTile(title: const Text('Activity Monitor')),
+
+              //risk history
+              new ExpansionTile(
+                title: const Text('Risk Hisotry'),
+                backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+                children: <Widget>[
+                  const ListTile(title: const Text('All')),
+                  const ListTile(title: const Text('Hypotension')),
+                  const ListTile(title: const Text('Hypothermia')),
+                  const ListTile(title: const Text('Pneumothorax'))
+                ],
               ),
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-                  child: new Text("Risk History", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/RiskHistory");}
-//                  onPressed: riskHistoryMenu
-                ),
+
+              //lab work
+              new ExpansionTile(
+                title: new Text('Lab Work'),
+                backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+                children: <Widget>[
+                  const ListTile(title: const Text('View Recommended Test')),
+                  const ListTile(title: const Text('Order Additional Test')),
+                  const ListTile(title: const Text('Enter Patient Report')),
+                  const ListTile(title: const Text('View Patient Report')),
+                  const ListTile(title: const Text('Enter Recommended Medication')),
+                ],
               ),
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-                  child: new Text("Lab Work", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/LabWork");}
-                ),
-              ),
-              new Container(
-                margin: new EdgeInsets.only(bottom: 1.0),
-                height: 50.0,
-                child: new RaisedButton(
-                  child: new Text("PSSAT Form", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
-                  onPressed: (){Navigator.of(context).pushNamed("/PSSATForm");}
-                ),
+
+              //PSSAT form
+              const ListTile(title: const Text('PSSAT Form')),
+
+              //STABLE
+              new ExpansionTile(
+                title: new Text('S.T.A.B.L.E'),
+                backgroundColor: Theme.of(context).accentColor.withOpacity(0.025),
+                children: <Widget>[
+                  const ListTile(title: const Text('Sugar')),
+                  const ListTile(title: const Text('Temperature')),
+                  const ListTile(title: const Text('Airway')),
+                  const ListTile(title: const Text('Blood Pressure')),
+                  const ListTile(title: const Text('Lab Work')),
+                  const ListTile(title: const Text('Emotional Support')),
+                ],
               )
             ],
           ),
+        ),
+        appBar: new AppBar(
+//        leading: new IconButton(
+//          icon: new Icon(Icons.menu),
+//          tooltip: 'Navigation menu',
+//          onPressed: null
+//        ),
+          title: new Text('Options', textAlign: TextAlign.left, style: new TextStyle(color: Colors.black)),
+        ),
+
+        // body is the majority of the screen.
+        body: new Container(
+            child: new Center(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Container(
+                      margin: new EdgeInsets.only(bottom: 1.0),
+                      height: 50.0,
+                      child: new RaisedButton(
+//                  child: new Icon(Icons.note),
+                          child: new Text("Current State", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                          onPressed: (){Navigator.of(context).pushNamed("/CurrentStatePage");}
+                      )
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(bottom: 1.0),
+                    height: 50.0,
+                    child: new RaisedButton(
+                      child: new Text("Risk Monitor", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                      onPressed: (){Navigator.of(context).pushNamed("/RiskMonitor");},
+                    ),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(bottom: 1.0),
+                    height: 50.0,
+                    child: new RaisedButton(
+                        child: new Text("Activity Monitor", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                        onPressed: (){Navigator.of(context).pushNamed("/ActivityMonitor");}
+                    ),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(bottom: 1.0),
+                    height: 50.0,
+                    child: new RaisedButton(
+                        child: new Text("Risk History", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                        onPressed: (){Navigator.of(context).pushNamed("/RiskHistory");}
+//                  onPressed: riskHistoryMenu
+                    ),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(bottom: 1.0),
+                    height: 50.0,
+                    child: new RaisedButton(
+                        child: new Text("Lab Work", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                        onPressed: (){Navigator.of(context).pushNamed("/LabWork");}
+                    ),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(bottom: 1.0),
+                    height: 50.0,
+                    child: new RaisedButton(
+                        child: new Text("PSSAT Form", style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+                        onPressed: (){Navigator.of(context).pushNamed("/PSSATForm");}
+                    ),
+                  )
+                ],
+              ),
+            )
         )
-      )
     );
   }
 }
@@ -215,3 +279,5 @@ class PSSATForm extends StatelessWidget{
     );
   }
 }
+
+
