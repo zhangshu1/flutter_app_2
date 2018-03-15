924aadbd34fa24e646774e694a06d09663b13b1d
SHU ZHANG <zhang.shu1@husky.neu.edu>
2018-03-15 12:20:40 -0400

    Some improvement on tiles

--- lib/TileMenu.dart
+++ lib/TileMenu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'Material/app.dart';
+import 'Material/all.dart';
 
 void main() {
   runApp(new MaterialApp(
